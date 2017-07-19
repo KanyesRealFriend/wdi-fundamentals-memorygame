9955ef813d90376f54099a12e4d215a98b266e54
Berman, Scott <Scott.Berman@capitalone.com>
2017-07-19 15:07:23 -0400

    created more game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,21 @@
 cards = ["queen", "queen", "king", "king"];
 cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log(cardsInPlay);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log(cardsInPlay);
-
-if (cardsInPlay.length === 2)
-  if(cardsInPlay[0] === cardsInPlay[1])
+var checkForMatch = function() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
-    else alert("Sorry, try again.");
-    
+  }else {
+    alert("Sorry, try again.");
+  }
+}
+
+var flipCard = function (cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+
+  if (cardsInPlay.length === 2)
+    checkForMatch();
+
+}
+flipCard(0);
+flipCard(2);
